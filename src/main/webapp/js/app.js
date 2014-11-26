5477dc612b43c092d93a82205e0b5b910fc2e85f
lucero rosadio <lucero.rosadioa@gmail.com>
2014-11-25 19:39:52 -0500

    add audiio
    proyecto final

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,12 +1,16 @@
 function AnimalesController($scope,$http) {
     $scope.animal={};
     $scope.animal.nombre='adivina';
+    $scope.audio = document.createElement('audio');
+
 
     $scope.mostrarAnimal = function(){
         console.log('sonido :'+$scope.sonido);
         $http.get('http://localhost:8080/animal?sonido='+$scope.sonido).
             success(function(data) {
             $scope.animal = data;
+                $scope.audio.src = 'mp3/'+$scope.animal.nombre+'.mp3';
+                $scope.audio.play();
         });
     };
 
